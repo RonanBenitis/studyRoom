5cc499c6abf3a19fe7519fd3c8077421ed82be11
RonanBenitis <ronanbenitis@hotmail.com>
2024-02-14 11:58:00 -0300

    update: Learning section concluded

---
+++ learning__web-programming/course_alura/js__crie-paginas-dinamicas/desafios/desafio02/enunciado.md
@@ -0,0 +1,8 @@
+No JavaScript podemos criar nossas próprias referências armazenarem de maneira mais simples valores para utilizarmos ao longo do código.
+
+Desta forma, a partir deste código no [Codepen](https://codepen.io/vanessametonini/pen/eYEVEqR), faça:
+- Uma referência que armazene uma lista com todas as teclas e imprima o valor dela na aba console.
+
+Como fazer:
+- Abra o link do CodePen;
+- Abra a aba JS e escreva seu código dentro dela;
--- learning__web-programming/course_alura/js__crie-paginas-dinamicas/mainProject/main.js
+++ learning__web-programming/course_alura/js__crie-paginas-dinamicas/mainProject/main.js
@@ -1,7 +1,14 @@
+// Snipet in JavaScript
+
 // Pom ------------------------------------------------
 function tocaSomPom(){
     // Bloco ou corpo da função
     document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+// Declaração de referência
+const listaDeTeclas = document.querySelectorAll('.tecla'); 
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+// Desafio: Automatizar a reprodução sem utilizar o método de copiar e colar para cada indice existente.
