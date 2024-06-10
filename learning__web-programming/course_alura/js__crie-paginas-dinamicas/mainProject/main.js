011b9f03688a254b579cb837a0271d284f4b7870
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-06-10 15:54:42 -0300

    update: conclusao do curso

--- learning__web-programming/course_alura/js__crie-paginas-dinamicas/mainProject/main.js
+++ learning__web-programming/course_alura/js__crie-paginas-dinamicas/mainProject/main.js
@@ -1,24 +1,42 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento =  document.querySelector(seletorAudio);
+
+    // Tratando possiveis erros de input
+    if (elemento && elemento.localName === 'audio') {
+        /*
+        Javascript interpreta, quando colocamos nenhuma condição (observe
+        o caso if(elemento)) para iniciar a condição caso o parametro
+        EXISTA, ou seja, quando o parametro É UM VALOR, ou seja, não for
+        igual a null, 0, vazio, undefined
+        */
+        elemento.play();
+    } else {
+        alert('Elemento inválido ou não encontrado');
+        console.log('Elemento inválido ou não encontrado');
+    }
 }
 
 // Declaração de referência
 const listaDeTeclas = document.querySelectorAll('.tecla'); 
 
-let i = 0;
-
-while(i < listaDeTeclas.length) {
+for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i]; // deixando código mais limpo
     const instrumento = tecla.classList[1];
 
     // Aplicando Template String
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function () {
-        tocaSom(idAudio) // concatenando com o
-                                        // valor obtido
+    tecla.onclick = function() {
+        tocaSom(idAudio);
     };
 
-    i++;
-    // console.log(i);
+    tecla.onkeydown = function(evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
