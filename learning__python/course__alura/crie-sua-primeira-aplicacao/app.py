66f1fd967285e7d4bc358ff7e44c353c686bb981
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-07-12 16:30:56 -0300

    py_alura: avançando no curso

--- learning__python/course__alura/crie-sua-primeira-aplicacao/app.py
+++ learning__python/course__alura/crie-sua-primeira-aplicacao/app.py
@@ -1,5 +1,13 @@
 import os
 
+# Definindo em escopo global
+# Lista de dicionarios
+restaurantes = [
+                {'nome':'a', 'categoria':'1', 'ativo':False},
+                {'nome':'b', 'categoria':'2', 'ativo':True},
+                {'nome':'c', 'categoria':'3', 'ativo':False},
+                ]
+
 def exibir_nome_do_programa():
     print("""
 
@@ -12,8 +20,11 @@ def exibir_nome_do_programa():
     """)
 
 def finalizar_app():
-    os.system('cls')
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizando o app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal ')
+    main()
 
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
@@ -21,18 +32,68 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
+def opcao_invalida():
+    print('Opção invalida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print() # Outra forma de pular linha
+
+def cadastrar_novo_reastaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    # Não realizamos o casting pois queremos que seja string mesmo
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar')
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False} 
+    restaurantes.append(dados_do_restaurante)
+    print(f'O restaurante: {nome_do_restaurante} foi cadastrado com sucesso')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        nome_resturante = restaurante['nome']
+        categoria_restaurante = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_resturante} | {categoria_restaurante} | {ativo}')
+
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            # Palavra reservada not (inversão)
+            restaurante['ativo'] = not restaurante['ativo']
+            # Utilizando ternário
+            print(f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante foi destivado com sucesso')
+            voltar_ao_menu_principal()
+            break
+
+    print(f'O restaurante {nome_restaurante} não foi encontrado')
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        
+        if opcao_escolhida == 1:
+            cadastrar_novo_reastaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            alternar_estado_restaurante()
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
     exibir_nome_do_programa()
