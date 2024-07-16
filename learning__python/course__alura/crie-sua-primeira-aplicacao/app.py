d5b50ab70ba8fa8bde50bb4b53bf929a266f7cfd
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-07-15 21:45:49 -0300

    py-alura: finalizacao de curso

--- learning__python/course__alura/crie-sua-primeira-aplicacao/app.py
+++ learning__python/course__alura/crie-sua-primeira-aplicacao/app.py
@@ -9,6 +9,7 @@ restaurantes = [
                 ]
 
 def exibir_nome_do_programa():
+    '''Exibir o logo do cliente no prompt ao iniciar o programa '''
     print("""
 
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -20,28 +21,63 @@ def exibir_nome_do_programa():
     """)
 
 def finalizar_app():
+    '''Chama a função exibir_subtitulo com o texto 'Finalizando o app'
+    encerrando a aplicação em seguida, por não chamar a
+    função main novamente
+    '''
     exibir_subtitulo('Finalizando o app')
 
 def voltar_ao_menu_principal():
+    '''Pede uma tecla pra voltar ao menu principal
+    
+    Outpus:
+    - Retorna ao menu principal
+    '''
     input('\nDigite uma tecla para voltar ao menu principal ')
     main()
 
 def exibir_opcoes():
+    '''Exibir as opção abaixo listadas no prompt
+    Essa função apenas exibi, não possui inputs
+    '''
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair\n')
 
 def opcao_invalida():
+    ''' Exibe mensagem de opção inválida e retorna ao menu principal 
+    
+    Outputs:
+    - Retorna ao menu principal
+    '''
     print('Opção invalida!\n')
     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
+    ''' Limpa o console e exibe um subtítulo estilizado na tela
+    
+    Inputs:
+    - texto: (str) O texto do subtítulo
+    '''
     os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print() # Outra forma de pular linha
 
 def cadastrar_novo_reastaurante():
+    '''Essa função é responsável por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adiciona um novo restaurante a lista de restaurantes
+
+    '''
     exibir_subtitulo('Cadastro de novos restaurantes')
     # Não realizamos o casting pois queremos que seja string mesmo
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar')
@@ -52,17 +88,32 @@ def cadastrar_novo_reastaurante():
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
+    ''' Lista os restaurantes presentes na lista 
+    
+    Outputs:
+    - Exibe a lista de restaurantes na tela
+    '''
     exibir_subtitulo('Listando os restaurantes')
+    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
 
     for restaurante in restaurantes:
         nome_resturante = restaurante['nome']
         categoria_restaurante = restaurante['categoria']
-        ativo = restaurante['ativo']
-        print(f'- {nome_resturante} | {categoria_restaurante} | {ativo}')
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_resturante.ljust(20)} | {categoria_restaurante.ljust(20)} | {ativo}')
 
     voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
+    ''' Altera o estado ativo/desativado de um restaurante. 
+    O usuário insere o nome do restaurante, voltando ao menu no final
+    da execução da função.
+
+    Outputs:
+    - Exibe mensagem indicando o sucesso da operação
+    ou
+    - Exibe mensagem indicando que não achou o restaurante
+    '''
     exibir_subtitulo('Alternando estado do restaurante')
     nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
 
@@ -79,6 +130,13 @@ def alternar_estado_restaurante():
     voltar_ao_menu_principal()
 
 def escolher_opcao():
+    ''' Solicita e executa a opção escolhida pelo usuário 
+    
+    Outputs:
+    - Executa a opção escolhida pelo usuário, sendo cada opção
+    uma função condizente com a opção escolhida.
+    - O titulo das opções é demonstrada por outra função
+    '''
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         
@@ -96,6 +154,7 @@ def escolher_opcao():
         opcao_invalida()
 
 def main():
+    ''' Função principal que inicia o programa '''
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
