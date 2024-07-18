b2c2328df40655cf33fc360ab1a677208c76557a
Ronan Ferraz <ronanbenitis@hotmail.com>
2024-07-18 20:08:06 -0300

    alura_csharp: inicio do curso

---
+++ learning__c#/course__alura/criando_primeira_aplicacao/Program.cs
@@ -0,0 +1,11 @@
+﻿// Screen Sound
+
+string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+Console.WriteLine(mensagemBoasVindas);
+
+string curso = "Criando o primeiro programa";
+string nome = "Ronan";
+string sobrenome = "Ferraz";
+
+Console.WriteLine("Guilherme");
+Console.WriteLine("Outro instrutor");
